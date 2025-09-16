1c2c87edd93ff8f49fffa8461d4fa31f5fdd7071
Prajin GN <prajin376@gmail.com>
2025-09-17 00:55:00 +0530

    Create TicTacToe.py
    added game logic and user interactivity.

---
+++ TicTacToe.py
@@ -0,0 +1,83 @@
+class game:
+    def __init__(self):
+        self.board = [[' ', ' ', ' '] for i in range(3)]
+        self.freeSpace = 9
+
+    def printBoard(self):
+        print()
+        print(f" {self.board[0][0]} | {self.board[0][1]} | {self.board[0][2]} ")
+        print("---|---|---")
+        print(f" {self.board[1][0]} | {self.board[1][1]} | {self.board[1][2]} ")
+        print("---|---|---")
+        print(f" {self.board[2][0]} | {self.board[2][1]} | {self.board[2][2]} ")
+
+    def runGame(self):
+        self.board = [[' ', ' ', ' '] for i in range(3)]
+        self.freeSpace = 9
+        player.playerNum = 1
+        p1 = player('X', self)
+        p2 = player('O', self)
+        status = None
+        while(True):
+            self.printBoard()
+            status = p1.play()
+            if status: 
+                break
+            self.printBoard()
+            status = p2.play()
+            if status:
+                break
+        
+        if status == "X" or status == "O":
+            playerName = p1.name if p1.symbol == status else p2.name
+            self.printBoard()
+            print(f"\nThe WINNER is: {playerName} ({status})")
+        else:
+            print("\nIt is a TIE")
+
+
+class player:
+    playerNum = 1
+    def __init__(self, symbol, gameObj):
+        self.name = input(f"Enter name of player {player.playerNum}: ").strip().capitalize()
+        self.symbol = symbol
+        self.gameObj = gameObj
+        player.playerNum += 1
+
+    def play(self):
+        print(f"{self.name}'s turn ({self.symbol}):")
+        while True:
+            self.x = int(input("Enter row #(1 - 3): ")) - 1
+            self.y = int(input("Enter column #(1 - 3): ")) - 1
+
+            if self.gameObj.board[self.x][self.y] == " ":
+                self.gameObj.board[self.x][self.y] = self.symbol
+                self.gameObj.freeSpace -= 1
+                break
+            else:
+                print("Enter valid slot!")
+        return self.getStatus()
+    
+    def getStatus(self):
+        for i in range(3):
+            if self.gameObj.board[i][0] == self.gameObj.board[i][1] == self.gameObj.board[i][2] == self.symbol:
+                return self.symbol
+        for i in range(3):
+            if self.gameObj.board[0][i] == self.gameObj.board[1][i] == self.gameObj.board[2][i] == self.symbol:
+                return self.symbol
+        if self.gameObj.board[0][0] == self.gameObj.board[1][1] == self.gameObj.board[2][2] == self.symbol:
+            return self.symbol
+        if self.gameObj.board[0][2] == self.gameObj.board[1][1] == self.gameObj.board[2][0] == self.symbol:
+            return self.symbol
+        
+        if self.gameObj.freeSpace == 0: return "tie"
+
+g = game()
+while True:
+    g.runGame()
+    playAgain = input("\nPlay Again? (Y / n): ").strip().lower()
+    if playAgain == 'n':
+        break
+    else:
+        print()
+        g.runGame()
